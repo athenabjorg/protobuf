b46df3338e3e3b80801a0080eaf5b4b4eb1588f1
athenabjorg <athenabjorg@gmail.com>
2018-08-21 13:29:05 +0000

    read and write message from file

--- simple/main.go
+++ simple/main.go
@@ -1,9 +1,74 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/athenabjorg/protobuf/simple/src/simple"
+)
+
 func main() {
-	doSimple()
+	sm := doSimple()
+
+	readAndWriteDemo(sm)
+
 }
 
-func doSimple() {
-	example_simple.SimpleMessage
+func readAndWriteDemo(sm proto.Message) {
+	writeToFile("simple.bin", sm)
+	sm2 := &simplepb.SimpleMessage{}
+	readFromFile("simple.bin", sm2)
+	fmt.Println(sm2)
+}
+
+func writeToFile(fname string, pb proto.Message) error {
+	out, err := proto.Marshal(pb)
+	if err != nil {
+		log.Fatalln("Can't serialise to bytes, err")
+		return err
+	}
+
+	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Can't write to file", err)
+		return err
+	}
+
+	fmt.Println("Data has been written")
+	return nil
+}
+
+func readFromFile(fname string, pb proto.Message) error {
+	in, err := ioutil.ReadFile(fname)
+	if err != nil {
+		log.Fatalln("Something went wront when reading the file", err)
+
+	}
+
+	err2 := proto.Unmarshal(in, pb)
+	if err2 != nil {
+		log.Fatalln("Couldn't put the bytes into the protocol buffers struct", err)
+		return err2
+	}
+
+	return nil
+}
+
+func doSimple() *simplepb.SimpleMessage {
+	sm := simplepb.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "My Simple Message",
+		SampleList: []int32{1, 4, 7, 8},
+	}
+
+	sm.Name = "I renamed you"
+
+	fmt.Println(sm)
+
+	fmt.Println("The ID is:", sm.Id)
+
+	return &sm
 }
