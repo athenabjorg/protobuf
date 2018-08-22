5dcc3b6d6fb1bb22c454e522dbeba1e15e104bf0
athenabjorg <athenabjorg@gmail.com>
2018-08-22 09:07:40 +0000

    proto examples finished

--- simple/main.go
+++ simple/main.go
@@ -5,16 +5,85 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/golang/protobuf/jsonpb"
+
 	"github.com/golang/protobuf/proto"
 
 	"github.com/athenabjorg/protobuf/simple/src/simple"
+
+	"github.com/athenabjorg/protobuf/simple/src/enum_example"
+	"github.com/athenabjorg/protobuf/simple/src/complex"
 )
 
 func main() {
 	sm := doSimple()
 
 	readAndWriteDemo(sm)
+	jsonDemo(sm)
 
+	doEnum()
+
+	doComplex()
+}
+
+func doComplex(){
+	cm := complexpb.DummyMessage{
+		OneDummy: &complexpb.DummyMessage{
+			Id: 1,
+			Name: "First Message"
+		},
+		MultipleDummy: []*complex.DummyMessage{
+			&complexpb.DummyMessage{
+				Id: 2,
+				Name: "Second Message"
+			},
+			&complexpb.DummyMessage{
+				Id: 3,
+				Name: "Third Message"
+			},
+		}
+		
+	}
+
+	fmt.Println(cm)
+}
+
+
+func doEnum() {
+	em := enumpb.EnumMessage{
+		Id:           42,
+		DayOfTheWeek: enumpb.DayOfTheWeek_THURSDAY,
+	}
+
+	em.DayOfTheWeek = enumpb.DayOfTheWeek_MONDAY
+
+	fmt.Println(em)
+}
+
+func jsonDemo(sm proto.Message) {
+	smAsString := toJSON(sm)
+
+	fmt.Println(smAsString)
+
+	sm2 := &simplepb.SimpleMessage{}
+	fromJSON(smAsString, sm2)
+	fmt.Println("Successfully created proto struct:", sm2)
+}
+
+func toJSON(pb proto.Message) string {
+	marshaler := jsonpb.Marshaler{}
+	out, err := marshaler.MarshalToString(pb)
+	if err != nil {
+		log.Fatalln("Can't convert to JSON", err)
+	}
+	return out
+}
+
+func fromJSON(in string, pb proto.Message) {
+	err := jsonpb.UnmarshalString(in, pb)
+	if err != nil {
+		log.Fatalln("Couldn't unmarshal the JSON into the pb struct")
+	}
 }
 
 func readAndWriteDemo(sm proto.Message) {
